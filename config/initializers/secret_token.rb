07bd4669411b47f19770a1db5ceba64426ce456d
Londerson <londerson@gmail.com>
2013-07-25 14:06:31 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minerva::Application.config.secret_token = '9ffcaddec4b04962f3b144dfcdd9f4e83212734388e4213b003d7fa8f3996f31751eb19a4be8902a70f0f845b3a83bdb670aebf903c32f79b944fd4c5336d6e4'

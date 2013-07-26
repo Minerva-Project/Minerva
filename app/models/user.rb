9b99e493efe9668fdea6afa127d18f7e92bd6a5b
Londerson <londerson@gmail.com>
2013-07-26 16:15:19 -0300

    add header in admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+
 end

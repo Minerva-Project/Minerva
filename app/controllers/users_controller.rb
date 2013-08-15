fd41b660ed46636ed4b8c986c907be13c4e8ab9e
Londerson <londerson@gmail.com>
2013-08-15 15:09:52 -0300

    refactoring...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, except: 'index'
 
   def index
+    @course = Course.find_by_id(params[:course_id])
     @users = User.all
   end
 

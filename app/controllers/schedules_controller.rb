5520cd8cf7e3b1960fae83052126721410551b80
Londerson <londerson@gmail.com>
2013-08-17 23:01:10 -0300

    bug fix editing schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,13 +1,11 @@
 class SchedulesController < ApplicationController
-  before_filter :load_course
+  before_filter :find_course
 
   def index
-    @course = Course.find_by_id(params[:course_id])
-    @schedule = Schedule.all
+    @schedules = Schedule.all
   end
 
   def show
-    @course = Course.find_by_id(params[:course_id])
     @schedule = Schedule.find_by_id(params[:id])
   end
 
@@ -24,47 +22,31 @@ class SchedulesController < ApplicationController
     @schedule = Schedule.new(params[:schedule])
     @schedule.course = course
 
-    respond_to do |format|
-      if @schedule.save
-        format.html { redirect_to manage_class_path(course), notice: 'Schedule was successfully created.' }
-        format.json { render json: @schedule, status: :created, location: @schedule }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
+    if @schedule.save
+       redirect_to course_path(course), notice: 'Schedule was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /schedules/1
-  # PUT /schedules/1.json
   def update
     @schedule = Schedule.find(params[:id])
 
-    respond_to do |format|
-      if @schedule.update_attributes(params[:schedule])
-        format.html { redirect_to @schedule, notice: 'Schedule was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
+    if @schedule.update_attributes(params[:schedule])
+     redirect_to course_schedule_path(@course), notice: 'Schedule was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /schedules/1
-  # DELETE /schedules/1.json
   def destroy
-    @course = Course.find(params[:course_id])
     @schedule = Schedule.find(params[:id])
     @schedule.destroy
 
-    respond_to do |format|
-      format.html { redirect_to course_schedule_path(@course)  }
-      format.json { head :no_content }
-    end
+    redirect_to course_schedule_path(@course)
   end
 
-  def load_course
+  def find_course
     @course = Course.find(params[:course_id])
   end
 end

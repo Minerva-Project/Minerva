bbd6ab0fa9357564231cf4cec9c5e94755c08eaa
Londerson Mapa <londerson@mapadaprova.com.br>
2014-01-29 23:16:55 -0200

    delete some lines in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -22,18 +22,9 @@ Minerva::Application.routes.draw do
     delete "logout", :to => "devise/sessions#destroy"
   end
 
- # match '/show_teacher_courses' => 'teachers#show_courses'
- # match '/manage/courses/:id' => 'classes#manage_class', as: 'manage_class'
-
-
- # match '/show_student_courses' => 'students#show_courses'
   match '/index' => 'manage#index'
   match '/teacher' => 'static_pages#teacher'
   match '/student' => 'static_pages#student'
   match '/course/:id', to: 'users#search', as: 'search'
-
-  #match '/classes/courses/:id' => 'classes#index',   as: 'classes_course'
-  #match '/courses/:course_id/schedules/:id'       => 'schedules#index', as: 'manage_schedules'
-  #match '/courses/:course_id/schedules/:id'       => 'schedules#index', as: 'schedules'
 
 end

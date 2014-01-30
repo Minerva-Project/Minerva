4d48bda98aea386c9a2d2ea22dffa48b0b77b7a7
Londerson Mapa <londerson@mapadaprova.com.br>
2014-01-29 22:02:16 -0200

    reduce size of date box

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,8 @@ class User < ActiveRecord::Base
                   :street_address, :city, :state, :country, :observation, :first_name, :last_name,
                   :remember_me
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-
 
   has_one :profile
   has_and_belongs_to_many :courses
